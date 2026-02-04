7e038ef5bb32532f4508f346713ce17af5ea975b
albert.farhat <albert.farhat@bankaudi.com.lb>
2026-02-04 18:45:01 +0200

    fixed migration

--- SmartTodoSolution/source/infrastructure/SmartTodo.Infrastructure/Migrations/20260204163255_AddTagsAndDependencies.Designer.cs
+++ SmartTodoSolution/source/infrastructure/SmartTodo.Infrastructure/Migrations/20260204163255_AddTagsAndDependencies.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SmartTodo.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using SmartTodo.Infrastructure.Persistence;
 namespace SmartTodo.Infrastructure.Migrations
 {
     [DbContext(typeof(TodoDbContext))]
-    partial class TodoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260204163255_AddTagsAndDependencies")]
+    partial class AddTagsAndDependencies
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
